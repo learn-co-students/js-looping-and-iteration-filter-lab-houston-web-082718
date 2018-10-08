bde9a5b5fb8b24f0cdce177fe3e3bda238e0ba79
Flatiron School <andrewsprague@outlook.com>
2018-10-08 10:16:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+    found_drivers = drivers.filter(driver => {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+    return found_drivers;
+};
+
+function fuzzyMatch (drivers, beginning) {
+    found_drivers = drivers.filter(driver => {
+        return driver.slice(0, beginning.length) === beginning;
+    });
+
+    return found_drivers;
+};
+
+function matchName (drivers, name) {
+    return drivers.filter(driver => {
+        return driver.name === name;
+    });
+};
